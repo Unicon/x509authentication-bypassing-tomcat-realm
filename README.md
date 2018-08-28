9f2f9bb248a00c4d277ef863aa0ed35dd5503674
axman <axman000@gmail.com>
2018-08-28 12:29:03 -0700

    updating documentation

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 x509authentication-bypassing-tomcat-realm
 ========================================
 
-This project contains custom implementation of Apache Tomcat 7 Realm whos primary purpose is to bypass the X509 client certificate's Subject field comparison against pre-configured Tomcat's authentication sources.
+This project contains custom implementation of Apache Tomcat 8 Realm whos primary purpose is to bypass the X509 client certificate's Subject field comparison against pre-configured Tomcat's authentication sources.
 
 This is useful in situations where web applications deployed in Tomcat have parts of their resources (URLs) protected by standard `security-constraint` mechanism with `CLIENT-CERT` login configuration that wish to just do the client-side X509 certificate authentication themselves or to delegate to an external authentication mechanism and not rely on the standard Tomcat's Realm to perform this step.
 
@@ -13,7 +13,7 @@ At the project's root directory from the command line simply execute `./gradlew`
 
 ### Install 
 
-Copy `build/libs/x509authentication-bypassing-tomcat-realm-1.0.jar` to `$CATALINA_HOME/lib` (where `$CATALINA_HOME` is the root directory of Tomcat 7 installation)
+Copy `build/libs/x509authentication-bypassing-tomcat-realm-1.0.jar` to `$CATALINA_HOME/lib` (where `$CATALINA_HOME` is the root directory of Tomcat 8 installation)
 
 ### Configure Realm
 
@@ -21,7 +21,14 @@ In `$CATALINA_HOME/conf/server.xml` add the following inside the `Engine` config
 
 ```xml
 <Realm className="org.apache.catalina.realm.CombinedRealm">      
-	<Realm className="net.unicon.tomcat7.realm.X509AuthenticationBypassingRealm"/>                              
+	<Realm className="net.unicon.tomcat8.realm.X509AuthenticationBypassingRealm"/>                              
+</Realm>
+```
+Note: If a "org.apache.catalina.realm.LockOutRealm" is already setup under the `Engine` configuration element, you can add it as a sub-realm.
+```xml
+<Realm className="org.apache.catalina.realm.LockOutRealm">      
+	<Realm className="net.unicon.tomcat8.realm.X509AuthenticationBypassingRealm"/>        
+	...                      
 </Realm>
 ```
 
@@ -31,7 +38,7 @@ In `$CATALINA_HOME/conf/server.xml` add the following inside the `Engine` config
 <security-constraint>
         <web-resource-collection>
             <web-resource-name>ALL</web-resource-name>
-            <url-pattern>/resource/that/needs/to/bypass/tomcat/x509/authentication</url-pattern>
+            <url-pattern>/login/cac</url-pattern>
         </web-resource-collection>
         <user-data-constraint>
             <transport-guarantee>CONFIDENTIAL</transport-guarantee>
--- src/main/java/net/unicon/tomcat8/realm/X509AuthenticationBypassingRealm.java
+++ src/main/java/net/unicon/tomcat8/realm/X509AuthenticationBypassingRealm.java
@@ -1,4 +1,4 @@
-package net.unicon.tomcat7.realm;
+package net.unicon.tomcat8.realm;
 
 import org.apache.catalina.realm.GenericPrincipal;
 import org.apache.catalina.realm.RealmBase;
@@ -6,17 +6,22 @@ import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 
 import java.security.Principal;
+import java.security.cert.X509Certificate;
 import java.util.Arrays;
 
 /**
- * This custom Tomcat7 Realm's primary purpose is to bypass the X509 client certificate's Subject field comparison against pre-configured Tomcat's authentication sources
- * when <code>security-constraint(s)</code> is set up for web applications URLs deployed in that Tomcat instance with <i>CLIENT-CERT</i> login config.
+ * This custom Tomcat8 Realm's primary purpose is to bypass the X509 client certificate's Subject
+ * field comparison against pre-configured Tomcat's authentication sources when
+ * <code>security-constraint(s)</code> is set up for web applications URLs deployed in that
+ * Tomcat instance with <i>CLIENT-CERT</i> login config.
  * <p/>
- * This is useful in cases where those application resources configured with <code>security-constraints</code> and <i>CLIENT-CERT</i> login config perform
- * authentication of client certificates themselves.
+ * This is useful in cases where those application resources configured with
+ * <code>security-constraints</code> and <i>CLIENT-CERT</i> login config perform authentication
+ * of client certificates themselves.
  * <p/>
- * This implementation just creates a valid instance of <code>GenericPrincipal</code> with username string containing the Subject line from client cert,
- * and default <i>X509</i> role - the one that must be configured inside <i>auth-constraint</i> element in applications' web.xml.
+ * This implementation just creates a valid instance of <code>GenericPrincipal</code> with
+ * username string containing the Subject line from client cert, and default <i>X509</i>
+ * role - the one that must be configured inside <i>auth-constraint</i> element in applications' web.xml.
  * The role name is configurable via this class' property of <code>authnConstraintRoleName</code>
  * <p/>
  * This implementation also turns off X509 certificate chain validation.
@@ -24,6 +29,9 @@ import java.util.Arrays;
  * @author Dmitriy Kopylenko
  * @author Unicon, inc.
  * @since 1.0
+ *
+ * Updated for Tomcat8.5 on 2018/08/13
+ *
  */
 public class X509AuthenticationBypassingRealm extends RealmBase {
 
@@ -39,21 +47,25 @@ public class X509AuthenticationBypassingRealm extends RealmBase {
 
     public X509AuthenticationBypassingRealm() {
         super();
+        log.debug("x509 - Running through Authentication bypass");
         this.setValidate(false);
     }
 
     @Override
     protected String getName() {
+        log.info("x509 - Getting Name through Authentication bypass");
         return this.getClass().getSimpleName();
     }
 
     @Override
     protected String getPassword(String username) {
+        log.info("x509 - Getting password through Authentication bypass");
         return null;
     }
 
     @Override
     protected Principal getPrincipal(String username) {
+        log.info("x509 - Getting Principal through Authentication bypass");
         if(log.isDebugEnabled()) {
             log.debug(String.format("Returning GenericPrincipal with username %s and role %s", username, this.authnConstraintRoleName));
         }
